3748f90db089dc8d0cd09e7419503fe68bdfbfc4
roufan <swe1704211@xmu.edu.my>
2019-11-26 19:51:38 +0800

    connected to database

--- AndroidStudioFile/app/src/main/java/com/example/hotelnest/MainActivity.java
+++ AndroidStudioFile/app/src/main/java/com/example/hotelnest/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -22,15 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
     private Button signUpButton;
     private Button signInButton;
-    private TextView textView;
-
+    private TextView message;
+    private EditText email;
+    private EditText password;
+    private String email_string;
+    private String password_string;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        message=findViewById(R.id.message);
+        email=findViewById(R.id.loginEmail);
+        password=findViewById(R.id.loginPassword);
         signUpButton = findViewById(R.id.signUpButton);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,44 +46,50 @@ public class MainActivity extends AppCompatActivity {
         });
 
         signInButton = findViewById(R.id.signInButton);
-        textView = findViewById(R.id.textView9);
+
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                String url ="http://192.168.1.107:8000/loginUser";
-
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                                textView.setText("Response is: "+ response);
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                        textView.setText("That didn't work!");
-                    }
-                })
-                {
-                    @Override
-                    protected Map<String, String> getParams()
-                    {
-                        Map<String, String>  params = new HashMap<String, String>();
-                        params.put("email", "SWE1704587@xmu.edu.my");
-                        params.put("password", "swe1704211");
-                        return params;
-                    }
-                };
-                queue.add(stringRequest);
+                email_string = email.getText().toString();
+                password_string = password.getText().toString();
 
 
-                openHomeActivity();
-            }
+                    RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                    String url = "http://10.0.2.2:8000/loginUser";
+
+                    // Request a string response from the provided URL.
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    // Display the first 500 characters of the response string.
+                                        if(response.equals("false")){
+                                            message.setText("The email or password is wrong!");
+                                        }else{
+                                            message.setText("Login Successful!");
+                                        }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            error.printStackTrace();
+
+                        }
+                    }) {
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<String, String>();
+                            params.put("email", email_string);
+                            params.put("password", password_string);
+                            return params;
+                        }
+                    };
+                    queue.add(stringRequest);
+
+
+                    //openHomeActivity();
+                }
+
         });
     }
 
