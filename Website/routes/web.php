3db5a416c6d06ee2df1a9779d69f2c7d30653786
Heng Jun Xi <viruskinghjx@hotmail.com>
2019-12-19 15:18:59 +0800

    correct web.php for createBooking api

--- Website/routes/web.php
+++ Website/routes/web.php
@@ -195,7 +195,7 @@ Route::post('createBooking',function(Request $request){
     try{
         return InsertBookingController::newBooking($request->fullName, $request->email, $request->phone, $request->icNum, 
         $request->checkInDate, $request->checkOutDate, $request->remark, $request->adult, $request->child,
-         $request->roomNum, $request->totalPrice, $request->hotelId, $request->roomId, $request->addBed);
+         $request->totalPrice, $request->hotelId, $request->roomId, $request->addBed);
     }catch(Exception $e){
         echo $e;
         return "false";
